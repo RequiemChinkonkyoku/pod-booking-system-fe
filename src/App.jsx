a439488add044fe5d1b370fed23ee550cdf599a8
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-09-18 23:41:39 +0700

    feat: added template asset files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "./App.css";
+import "./assets/css/material-dashboard.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +28,78 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header card-header-icon card-header-rose">
+            <div class="card-icon">
+              <i class="material-icons">assignment</i>
+            </div>
+            <h4 class="card-title ">Simple Table</h4>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead class=" text-primary">
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Country</th>
+                    <th>City</th>
+                    <th>Salary</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Dakota Rice</td>
+                    <td>Niger</td>
+                    <td>Oud-Turnhout</td>
+                    <td>$36,738</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Minerva Hooper</td>
+                    <td>Curaçao</td>
+                    <td>Sinaai-Waas</td>
+                    <td>$23,789</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Sage Rodriguez</td>
+                    <td>Netherlands</td>
+                    <td>Baileux</td>
+                    <td>$56,142</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Philip Chaney</td>
+                    <td>Korea, South</td>
+                    <td>Overland Park</td>
+                    <td>$38,735</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>Doris Greene</td>
+                    <td>Malawi</td>
+                    <td>Feldkirchen in Kärnten</td>
+                    <td>$63,542</td>
+                  </tr>
+                  <tr>
+                    <td>6</td>
+                    <td>Mason Porter</td>
+                    <td>Chile</td>
+                    <td>Gloucester</td>
+                    <td>$78,615</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
