13a25111e937184eda17e88b199767a2e850e816
RequiemRekuiemu <phuongdo0207@gmail.com>
2024-10-06 08:27:57 +0700

    feat: integrated register api and verify otp page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, message } from "antd";
+import axios from "../utils/axiosConfig";
 import "../css/Register.css";
 
 const RegisterPage = () => {
   const [form] = Form.useForm();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      // Use params to send the data as query parameters instead of the request body
+      const response = await axios.post("/Auth/Register/Customer", {
+        name,
+        email,
+        password,
+        roleId: "1",
+      });
+
+      // Store token if registration is successful
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      message.success("Registration successful!");
+
+      console.log("Registration successful: ", response.data);
+    } catch (error) {
+      console.error("Registration failed: ", error);
+      message.error("Registration failed. Please try again.");
+    }
+  };
 
   return (
     <div className="register-container">
@@ -14,23 +41,46 @@ const RegisterPage = () => {
         <Form
           form={form}
           name="register"
-          // onFinish={handleSubmit}
+          onFinish={handleSubmit}
           layout="vertical"
         >
           <Form.Item
-            name="username"
+            name="name"
             rules={[
               {
                 required: true,
-                message: "Please input your username!",
+                message: "Please input your name!",
               },
               {
-                pattern: /^[a-zA-Z0-9]+$/,
-                message: "Username can only contain letters and numbers!",
+                pattern: /^[a-zA-Z0-9\s]+$/,
+                message: "Name can only contain letters, numbers, and spaces!",
               },
             ]}
           >
-            <Input placeholder="Username" />
+            <Input
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+              {
+                type: "email",
+                message: "Please enter a valid email address!",
+              },
+            ]}
+          >
+            <Input
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Item>
           <Form.Item
             name="password"
@@ -39,12 +89,30 @@ const RegisterPage = () => {
                 required: true,
                 message: "Please input your password!",
               },
+              {
+                min: 8,
+                message: "Password must be at least 8 characters long!",
+              },
+              {
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+                message:
+                  "Password must contain at least one letter and one number.",
+              },
             ]}
           >
-            <Input.Password placeholder="Password" />
+            <Input.Password
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Form.Item>
           <Form.Item>
-            <Button htmlType="submit" className="register-button" block>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="register-button"
+              block
+            >
               Register
             </Button>
           </Form.Item>
---
+++ src/pages/VerifyOtpPage.jsx
@@ -0,0 +1,88 @@
+import { Button, Form, Input } from "antd";
+import axios from "../utils/axiosConfig";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/Login.css";
+
+const VerifyOtpPage = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  // const [credentials, setCredentials] = useState({
+  //   email: "",
+  //   password: "",
+  // });
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [form] = Form.useForm();
+
+  const handleSubmit = async () => {
+    try {
+      // Use query parameters as per your backend requirement
+      const response = await axios.post(`/Auth/Verify/${otp}?email=${email}`);
+
+      // Store token if login is successful
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      console.log("Verification successful");
+    } catch (error) {
+      console.error("Verification failed: ", error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-bg" />
+      <div className="login-form">
+        <h2>VERIFY ACCOUNT</h2>
+        <Form
+          form={form}
+          name="login"
+          onFinish={handleSubmit} // Keep onFinish for form submission
+          layout="vertical"
+        >
+          <Form.Item
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} // Set email value on change
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+              {
+                type: "email", // Ensure the value is a valid email format
+                message: "Please enter a valid email!",
+              },
+            ]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)} // Set password value on change
+            rules={[
+              {
+                required: true,
+                message: "Please input your OTP!",
+              },
+            ]}
+          >
+            <Input placeholder="OTP" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-button"
+              block
+            >
+              VERIFY
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyOtpPage;
